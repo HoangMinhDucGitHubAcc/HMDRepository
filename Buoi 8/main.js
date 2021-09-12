7150ad1643cfef5b59bcb0ff1f6be03100b4796d
HoangMinhDucGitHubAcc <90512978+HoangMinhDucGitHubAcc@users.noreply.github.com>
2021-09-12 22:12:12 +0700

    Buoi 8 JsBasic

---
+++ Buoi 8/main.js
@@ -0,0 +1,13 @@
+//console.log("hi")
+//let test = document.getElementById("div-test")
+//console.dir(test)
+//test.style 
+//test.textContent = "Xin chao"
+//test.outerHTML`<div id = "container"><div id = "d">`
+//let btn = document.getElementById(btn)
+//console.dir(btn)
+//btn.addEventListener("click", function () {})
+//function printTest() {
+//    console.log("test")
+//}
+//printTest()
