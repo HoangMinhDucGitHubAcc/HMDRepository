29835cc55c8b8991bc727bee5afcf39f2d1b8c82
HoangMinhDucGitHubAcc <90512978+HoangMinhDucGitHubAcc@users.noreply.github.com>
2021-09-12 22:10:49 +0700

    Buoi 6 JsBasic

---
+++ Buoi 6/main.js
@@ -0,0 +1,19 @@
+let list= {
+name :"An",
+phone: 12345678,
+address: "Kim Mã, Hà Nội",
+career:"Teacher",
+};
+//console.log(list)
+//console.log(list.address)
+let test = {
+    nameList: ["An", "Hoàng", "Dũng", "Hiếu"],
+    phoneList: [12345, 8230, 5204, 0934]
+ };
+ console.log(list.phone + test.phoneList[0])
+ console.log(list.phone + test.phoneList[1])
+ console.log(list.phone + test.phoneList[2])
+ console.log(list.phone + test.phoneList[3])
+ let bigArray = [1, 2, 3, 4, 5, 6]
+ for (let index = 0; index < bigArray.length; index = index + 1){}
+ console.log(bigArray)
